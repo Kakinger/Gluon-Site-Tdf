1c82638f6e8c9f40404ae4ae7aadddbac5f75936
Delta / Stefan <shuebbel@gmx.de>
2015-10-28 19:48:20 +0100

    l2tp

--- site.mk
+++ site.mk
@@ -5,40 +5,46 @@
 #		The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
 
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-14 \
+	gluon-mesh-batman-adv-15 \
 	gluon-alfred \
 	gluon-announced \
+	gluon-neighbour-info \
 	gluon-autoupdater \
+	gluon-setup-mode \
 	gluon-authorized-keys \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-contact-info \
 	gluon-config-mode-core \
-	gluon-config-mode-geo-location \
+	gluon-config-mode-autoupdater \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-contact-info \
+	gluon-migrate-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
+    gluon-ebtables-filter-roguenets \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
 	gluon-luci-private-wifi \
+	gluon-luci-node-role \
 	gluon-luci-wifi-config \
 	gluon-next-node \
-	gluon-mesh-vpn-fastd \
+    gluon-mesh-vpn-tunneldigger \
 	gluon-radvd \
-	gluon-setup-mode \
 	gluon-status-page \
-	haveged \
+	iwinfo \
 	iptables \
-	iwinfo
-
+	haveged \
+    tro-netwatch \
+    tro-offline_ssid
 ##	DEFAULT_GLUON_RELEASE
+
 #		version string to use for images
 #		gluon relies on
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.6+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.90+nightly+$(shell date '+%Y%m%d%H%M')+l2tp
 
 
 ##	GLUON_RELEASE
@@ -48,10 +54,13 @@ DEFAULT_GLUON_RELEASE := 0.6+exp$(shell date '+%Y%m%d')
 #		would generate images named like this:
 #			gluon-ff%site_code%-23.42+5-%router_model%.bin
 
+
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 # Default priority for updates.
+
+
 GLUON_PRIORITY ?= 0
 
 # Languages to include
